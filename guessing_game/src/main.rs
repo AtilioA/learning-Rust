ba5955f567edb56f2cbaaa519ab274d773e43f5e
AtilioA <atiliogpg@protonmail.com>
2021-10-20 16:15:44 -0300

    Add loop to guessing game, improve code
    Modularize code and catch edge cases

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,14 +1,13 @@
 use rand::Rng;
-use std::io;
 use std::cmp;
+use std::io;
 
-fn main() {
-    println!("Guess the number!");
-
-    let secret_number: u8 = rand::thread_rng().gen_range(1..=100);
-
-    println!("Secret number is: {}.", secret_number);
-    println!("Please input your guess (0-100):");
+fn read_input(min: u8, max: u8) -> String {
+    println!(
+        "Please input your guess ({min}-{max}):",
+        min = min,
+        max = max
+    );
 
     let mut guess = String::new();
 
@@ -16,13 +15,62 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read the input.");
 
-    let guess: u8 = guess.trim().parse().expect("Please type a number (0-100)!");
+    guess = guess.trim().to_string();
 
-    println!("Your guess is: {}.", guess);
+    return guess;
+}
 
-    match guess.cmp(&secret_number) {
-        cmp::Ordering::Less => println!("Too small!"),
-        cmp::Ordering::Greater => println!("Too big!"),
-        cmp::Ordering::Equal => println!("You guessed right!"),
+fn print_invalid_input(min: u8, max: u8) {
+    println!(
+        "Invalid input! A valid input is a number between {min} and {max}.\n",
+        min = min,
+        max = max
+    );
+}
+
+fn guess(secret_number: u8, min: u8, max: u8) {
+    loop {
+        let guess: u8 = match read_input(min, max).parse() {
+            // Guess must be a number.
+            Ok(num) => num,
+            Err(_) => {
+                print_invalid_input(min, max);
+                continue;
+            }
+        };
+
+        if guess < min || guess > max {
+            // Guess must be between min and max.
+            print_invalid_input(min, max);
+            continue;
+        }
+
+        println!("Your guess is: {}.", guess);
+
+        match guess.cmp(&secret_number) {
+            cmp::Ordering::Less => println!("Too small!"),
+            cmp::Ordering::Greater => println!("Too big!"),
+            cmp::Ordering::Equal => {
+                println!("You guessed right!");
+                break; // Exit guess loop.
+            }
+        }
     }
+}
+
+fn guessing_game(min: u8, max: u8) {
+    // Generate random number between range within the current thread of execution
+    let secret_number: u8 = rand::thread_rng().gen_range(min..=max);
+
+    guess(secret_number, min, max);
+}
+
+fn main() {
+    // Limits for the guessing game.
+    const MIN: u8 = 1;
+    const MAX: u8 = 100;
+
+    println!("Guess the number!\n");
+
+    guessing_game(MIN, MAX);
 }
