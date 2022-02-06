e2ba8f7131cfdf539790c747c3474a8352332852
AtilioA <atiliogpg@protonmail.com>
2022-02-06 13:25:45 -0300

    Add Exercism's "Semi Structured Logs"

---
+++ exercism/rust/semi-structured-logs/src/lib.rs
@@ -0,0 +1,30 @@
+// This stub file contains items which aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+#![allow(unused)]
+
+/// various log levels
+#[derive(Clone, PartialEq, Debug)]
+pub enum LogLevel {
+    Info,
+    Warning,
+    Error,
+}
+
+/// primary function for emitting logs
+pub fn log(level: LogLevel, message: &str) -> String {
+    match level {
+        LogLevel::Info => info(message),
+        LogLevel::Warning => warn(message),
+        LogLevel::Error => error(message),
+    }
+}
+
+pub fn info(message: &str) -> String {
+    return format!("[INFO]: {message}", message = message);
+}
+pub fn warn(message: &str) -> String {
+    return format!("[WARNING]: {message}", message = message);
+}
+pub fn error(message: &str) -> String {
+    return format!("[ERROR]: {message}", message = message);
+}
